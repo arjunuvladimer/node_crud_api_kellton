3a4f31c449c2caa3b73e9809479b0c405235cbe6
arjunuvladimer <arjunuvladimer@gmail.com>
2022-06-14 20:29:52 +0530

    Initial API setup

---
+++ index.js
@@ -0,0 +1,12 @@
+// Internal Modules / Dependencies => Classes, Functions
+// External or Third Party Modules/Dependencies => Classes, Functions
+
+// Relative Paths => Short Path
+// Absolute Paths => Long Path or Full Path
+const express = require('express')
+const app = express() // Functional Constructor
+
+
+app.listen(3000, () =>{
+    console.log("Successfully Running on Port 3000")
+})
